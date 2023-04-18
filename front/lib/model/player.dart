b73685be28bfa282b2f0b577229ca0f5c75b46fb
quentin.pergeline <quentin.pergeline@iwecloud.com>
2023-04-18 21:55:39 +0200

    front: init next.js app

--- front/lib/model/player.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter/foundation.dart';
-
-part 'player.freezed.dart';
-
-@freezed
-abstract class Player with _$Player {
-  factory Player({int id, String name, int score}) = _Player;
-}
--- front/lib/redux/player/actions.dart
+++
@@ -1,6 +0,0 @@
-class PlayerScoreUpdatedAction {
-  final int playerId;
-  final int scoreDiff;
-
-  const PlayerScoreUpdatedAction({this.playerId, this.scoreDiff});
-}
